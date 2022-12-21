9c15b99e7343c395cdb6453ca07f1391a0269599
Shawn-Z <sshawn309@protonmail.com>
2022-12-21 19:00:14 +0800

    bionic remote env

---
+++ docker/dockerfile/build_bionic_remote_env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+rm -rf build
+mkdir build
+cp -r ../../installers/* build
+cp -r ../../utilities/* build
+echo "#!/bin/bash" >> build/build.sh
+echo "set -e" >> build/build.sh
+echo "bash change_apt_source.sh" >> build/build.sh
+echo "bash change_ros_source.sh" >> build/build.sh
+echo "bash ssh_for_remote_dev.sh" >> build/build.sh
+echo "bash install_dependencies.sh" >> build/build.sh
+
+docker build -f bionic_remote_env.Dockerfile -t sshawn/bionic_remote_env:1.0 .
