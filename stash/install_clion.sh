d6a5dc0e569b7cb7c1df4ac83fcdc7c502573859
sshawn9 <sshawn309@protonmail.com>
2023-02-11 23:55:54 +0800

    update_install_clion

--- stash/install_clion.sh
+++ stash/install_clion.sh
@@ -1,13 +1,13 @@
- #!/bin/bash
+#!/bin/bash
 set -e
 
-if [ -f "software_package/CLion"* ]; then
+if [ -f "CLion"* ]; then
 	echo "find clion package, installing..."
 else
 	echo "can't find clion package, exit"
 	exit
 fi
 #sudo cp software_package/CLion*.tar.gz /usr/local/
-sudo tar -zxf software_package/CLion*.tar.gz -C /usr/local/
-echo "alias clion=/usr/local/clion*/bin/clion.sh" >> ~/.bashrc
-source ~/.bashrc
+sudo tar -zxf CLion*.tar.gz -C /usr/local/
+echo "alias clion=/usr/local/clion*/bin/clion.sh" >> ~/.bash_aliases
+source ~/.bash_aliases
