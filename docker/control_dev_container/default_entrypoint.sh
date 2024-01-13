31a90f7a6ef82ed6be99fce8c62572dba9fd5256
shawn <sshawn309@protonmail.com>
2024-01-13 22:39:06 +0800

    Update default_entrypoint.sh

--- docker/control_dev_container/default_entrypoint.sh
+++ docker/control_dev_container/default_entrypoint.sh
@@ -3,5 +3,16 @@
 set -e
 
 source /opt/ros/$ROS_DISTRO/setup.bash
-source /msg_ws/devel/setup.bash
+
+if [ $ROS_VERSION == "1" ]; then
+    if [ -f /msg_ws/devel/setup.bash ]; then
+        source /msg_ws/devel/setup.bash
+    fi
+fi
+if [ $ROS_VERSION == "2" ]; then
+    if [ -f /msg_ws/install/setup.bash ]; then
+        source /msg_ws/install/setup.bash
+    fi
+fi
+
 exec "$@"
