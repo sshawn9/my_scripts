3fa22389411ef186aaa4c5033fc088b5f67bd719
shawn <sshawn309@protonmail.com>
2024-01-14 00:17:25 +0800

    Update start.sh

--- docker/control_dev_container/start.sh
+++ docker/control_dev_container/start.sh
@@ -1,8 +1,18 @@
-docker run -it \
+#!/bin/bash
+
+set -e
+
+tag="humble_msgs_20240113"
+tag="melodic_msgs_20240113"
+tag="noetic_msgs_20240113"
+image="sshawn/control_dev:$tag"
+
+docker run -itd \
            -v ~:/star \
            -v ~/Documents/GitHub:/GitHub \
-           -v ~/Documents/Github/autonomous:/autonomous \
-           --name control_dev \
-           --hostname control_dev \
-           sshawn/control_dev:noetic_msgs_20231205 \
+           -v ~/Documents/Github/autonomous:/root/autonomous \
+           -v ~/Documents/Github/autonomous/catkin_ws/src:/src \
+           --name $tag \
+           --hostname $tag \
+           $image \
            bash
