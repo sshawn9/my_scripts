203be25a0d56851ef06f375f902841768b1b15de
shawn <sshawn309@protonmail.com>
2024-01-17 00:20:23 +0800

    try again

--- installers/control_dev_install.sh
+++ installers/control_dev_install.sh
@@ -30,10 +30,12 @@ $DO apt-get install -y wget \
                        libfmt-dev \
                        libspdlog-dev \
                        libprotobuf-dev \
+                       libprotoc-dev \
+                       liblmdb++-dev
 
-# install_protobuf_url=https://raw.githubusercontent.com/sshawn9/my_scripts/master/installers/install_protobuf.sh
-# install_protobuf_url=https://raw.kkgithub.com/sshawn9/my_scripts/master/installers/install_protobuf.sh
+install_protobuf_url=https://raw.githubusercontent.com/sshawn9/my_scripts/master/installers/install_protobuf.sh
+install_protobuf_url=https://raw.kkgithub.com/sshawn9/my_scripts/master/installers/install_protobuf.sh
 
-# wget -t 10 $install_protobuf_url
-# $DO bash install_protobuf.sh
-# rm -rf install_protobuf.sh
+wget -t 10 $install_protobuf_url
+$DO bash install_protobuf.sh
+rm -rf install_protobuf.sh
