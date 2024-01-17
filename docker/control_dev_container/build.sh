dc55587ec83a3b7be009a22379fa22fecc76866a
shawn <sshawn309@protonmail.com>
2024-01-17 11:56:08 +0800

    no cache build

--- docker/control_dev_container/build.sh
+++ docker/control_dev_container/build.sh
@@ -6,12 +6,12 @@ current_date=$(date +%Y%m%d)
 
 BASE_IMAGE=ros:melodic-ros-core
 image_name=sshawn/control_dev:melodic_msgs_$current_date
-docker build --build-arg BASE_IMAGE=$BASE_IMAGE -t $image_name .
+docker build --build-arg BASE_IMAGE=$BASE_IMAGE --no-cache -t $image_name .
 
 BASE_IMAGE=ros:noetic-ros-core
 image_name=sshawn/control_dev:noetic_msgs_$current_date
-docker build --build-arg BASE_IMAGE=$BASE_IMAGE -t $image_name .
+docker build --build-arg BASE_IMAGE=$BASE_IMAGE --no-cache -t $image_name .
 
 BASE_IMAGE=ros:humble-ros-base
 image_name=sshawn/control_dev:humble_msgs_$current_date
-docker build --build-arg BASE_IMAGE=$BASE_IMAGE -t $image_name .
+docker build --build-arg BASE_IMAGE=$BASE_IMAGE --no-cache -t $image_name .
