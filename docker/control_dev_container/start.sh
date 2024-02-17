711a4dc18134dbce0eb05b4a928c6ce444d904a2
shawn <sshawn309@protonmail.com>
2024-02-17 17:47:31 +0800

    Update start.sh

--- docker/control_dev_container/start.sh
+++ docker/control_dev_container/start.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-tag="humble_msgs_20240117"
-tag="melodic_msgs_20240117"
-tag="noetic_msgs_20240117"
+tag="humble_msgs_20240217"
+tag="melodic_msgs_20240217"
+tag="noetic_msgs_20240217"
 image="sshawn/control_dev:$tag"
 
 docker run -itd \
@@ -12,6 +12,8 @@ docker run -itd \
            -v ~/Documents/GitHub:/GitHub \
            -v ~/Documents/Github/autonomous:/root/autonomous \
            -v ~/Documents/Github/autonomous/workspace/src:/src \
+           -v /etc/localtime:/etc/localtime:ro \
+           --privileged \
            --name $tag \
            --hostname $tag \
            $image \
