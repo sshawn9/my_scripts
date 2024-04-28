9fb3455a4449e543e95a13f769433df34aee18c3
shawn <sshawn309@protonmail.com>
2024-04-29 02:38:24 +0800

    Update change_apt_source.sh

--- utilities/change_apt_source.sh
+++ utilities/change_apt_source.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# change apt source to tsinghua on ubuntu
-
-# ---
-# test information
-# test passed on docker image ubuntu:18.04
-# ---
-
 set -e
 
 # check if root
@@ -16,5 +9,5 @@ if [ $UID -eq 0 ];then DO="";fi
 
 cd /etc/apt
 $DO cp sources.list sources.list.back
-$DO sed -i 's/archive.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g'  /etc/apt/sources.list
-$DO sed -i 's/security.ubuntu.com/mirrors.tuna.tsinghua.edu.cn/g'  /etc/apt/sources.list
+$DO sed -i 's|http://.*.ubuntu.com|http://mirrors.tuna.tsinghua.edu.cn|g'  /etc/apt/sources.list
+$DO sed -i 's|security.ubuntu.com|mirrors.tuna.tsinghua.edu.cn|g'  /etc/apt/sources.list
